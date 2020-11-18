ab0c388149437c5e9cc623dcfdc6160158e40e92
Lukas Gresham <69813309+LukasGG1@users.noreply.github.com>
2020-11-18 16:38:29 -0600

    Add files via upload

---
+++ RPG/Map1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+using MathLibrary;
+
+namespace RPG
+{
+    class Map1 : Actor
+    {
+        private Sprite _sprite;
+        public Map1(float x, float y, char icon = ' ', ConsoleColor color = ConsoleColor.White)
+           : base(x, y, icon, color)
+        {
+            _sprite = new Sprite("Images/Map.png");
+        }
+
+        /// <param name="x">Position on the x axis</param>
+        /// <param name="y">Position on the y axis</param>
+        /// <param name="rayColor">The color of the symbol that will appear when drawn to raylib</param>
+        /// <param name="icon">The symbol that will appear when drawn</param>
+        /// <param name="color">The color of the symbol that will appear when drawn to the console</param>
+        public Map1(float x, float y, Color rayColor, char icon = ' ', ConsoleColor color = ConsoleColor.White)
+            : base(x, y, rayColor, icon, color)
+        {
+            _sprite = new Sprite("Images/Map.png");
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+            base.Update(deltaTime);
+        }
+
+        public override void Draw()
+        {
+            _sprite.Draw(_globalTransform);
+            base.Draw();
+        }
+    }
+}
